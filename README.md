749f770effe0912a248a885e02df80bab5542d69
Pooryaa Cheraaqee <p.cheraaghi@gmail.com>
2022-12-25 23:59:15 +0330

    temp

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 (```Look for .pdf and .mp4 files```)
 # LaTeX
 I installed ```texlive-full``` using [this tutorial](https://fahim-sikder.github.io/post/installing-texlive-latest-ubuntu/) on Ubuntu 20.04.
+# Your Status
+You may get a load of your scoring status on ```./status/your_name/your_name.pdf```  
+*IT IS NOT NECESSARILY UPDATED AT ANY MOMENT*
---
+++ status/script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+while read name
+do
+	# cd /home/pooryaa/presenting_concepts/status
+	# mkdir -p $name/source
+	# cp /home/pooryaa/mindmap/mindmap.tex ./$name/source/$name.tex
+	cd /home/pooryaa/presenting_concepts/status/$name/source
+	# sed -i 's/'"$name"'/'"$name"'\\\\ \$\\frac{?}{20}\$/' $name.tex
+	xelatex $name.tex
+	mv $name.pdf ..
+done < listen.txt
