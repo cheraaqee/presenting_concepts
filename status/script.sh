ec8ea94bae60e9182d7752d359bb50b98a4265a9
Pooryaa Cheraaqee <p.cheraaghi@gmail.com>
2023-01-11 16:17:54 +0330

    rating

--- status/script.sh
+++ status/script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 while read name
 do
-	# cd /home/pooryaa/presenting_concepts/status
+	cd /home/pooryaa/presenting_concepts/status/$name/source
 	# mkdir -p $name/source
 	# cp /home/pooryaa/mindmap/mindmap.tex ./$name/source/$name.tex
-	cd /home/pooryaa/presenting_concepts/status/$name/source
+	# cd /home/pooryaa/presenting_concepts/status/$name/source
 	# sed -i 's/'"$name"'/'"$name"'\\\\ \$\\frac{?}{20}\$/' $name.tex
-	xelatex $name.tex
+	# xelatex $name.tex
 	mv $name.pdf ..
 done < listen.txt
